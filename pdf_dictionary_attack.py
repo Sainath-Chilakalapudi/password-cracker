e018ca60e8ae2f3880b3eab0f183b66037fbcfd4
Sainath-Chilakalapudi <93853110+Sainath-Chilakalapudi@users.noreply.github.com>
2023-07-06 11:43:57 +0530

    pdf dictionary attack 2
    made it into a functon

--- pdf_dictionary_attack.py
+++ pdf_dictionary_attack.py
@@ -1,17 +1,24 @@
 import pikepdf
 from tqdm import tqdm
 
-# load password list
-passwords = [ line.strip() for line in open("passwords.txt") ]
+def decrypt_pdf_with_password_list(passwords_file, pdf_file):
+    # Load password list
+    passwords = [line.strip() for line in open(passwords_file)]
 
-# iterate over passwords
-for password in tqdm(passwords, "Decrypting PDF"):
-    try:
-        # open PDF file
-        with pikepdf.open("protected.pdf", password=password) as pdf:
-            # Password decrypted successfully, break out of the loop
-            print("[+] Password found:", password)
-            break
-    except pikepdf._core.PasswordError as e:
-        # wrong password, just continue in the loop
-        continue
+    # Iterate over passwords
+    for password in tqdm(passwords, "Decrypting PDF"):
+        try:
+            # Open PDF file
+            with pikepdf.open(pdf_file, password=password) as pdf:
+                # Password decrypted successfully, break out of the loop
+                print("[+] Password found:", password)
+                break
+        except pikepdf._core.PasswordError as e:
+            # Wrong password, continue to the next password
+            continue
+
+    print("[-] Password not found.")
+
+
+# Usage example
+decrypt_pdf_with_password_list("passwords.txt", "protected.pdf")
